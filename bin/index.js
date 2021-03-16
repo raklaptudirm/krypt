12054ceb513db68e8a0d05249f69c2f51fe4817f
Rak Laptudirm <68542775+raklaptudirm@users.noreply.github.com>
2021-03-16 16:55:44 +0530

    Fixed password Bug
    Fixed password bug which could cause letters to be undefined. Re-indented code.

--- bin/index.js
+++ bin/index.js
@@ -17,122 +17,122 @@ let _COMMS = [{name:'edit ', use: 'Edit; Edit a password', ex: 'edit <pass_id>'}
 
 async function main() {
 	if (fs.existsSync(__dirname + '/../lib/database.json')) {
-	  loadDatabase()
-	  _KEY = crypt.SHA_hash(readlineSync.question('PASSWORD: ', { hideEchoBack: true }))
-	  if (_DATABASE.checksum === crypt.SHA_hash(_KEY)) {
-	    console.log(chalk.green.bold('Logged in.'))
-	    loadPasswords()
-	    while (true) {
-	      console.log('')
-	      let input = readlineSync.prompt()
-	      if (input === 'exit') {
-	        break
-	      } else if (input === 'cmast') {
-	        _KEY = crypt.SHA_hash(readlineSync.questionNewPassword())
-	        _DATABASE.checksum = crypt.SHA_hash(_KEY)
-	        reEncryptData()
-	      } else if (input === 'npass') {
-	        const name_ = readlineSync.question('Password Name: ')
-	        const username_ = readlineSync.question('Username: ')
-	        const password_ = readlineSync.question('Password (leave empty to generate): ', { hideEchoBack: true }) || generatePassword()
-	        _PASSWORDS.push(createPass(name_, username_, password_))
-	        console.log(chalk.green.bold(`Sucessfully added password at ID:${_PASSWORDS.length}.`))
-	        reEncryptData()
-	        loadDatabase()
-	        loadPasswords()
-	      } else if (input.startsWith('gent')) {
-	        input = parseInt(input.slice(4)) - 1
-	        if (input === undefined || isNaN(input) || input < 0 || input >= _PASSWORDS.length) {
-	          console.log(chalk.red.bold('ID out of bounds.'))
-	        } else {
-	          printPass(_PASSWORDS[input], input + 1)
-	        }
-	      } else if (input.startsWith('gpass')) {
-	        input = parseInt(input.slice(5)) - 1
-	        if (input === undefined || isNaN(input) || input < 0 || input >= _PASSWORDS.length) {
-	          console.log(chalk.red.bold('ID out of bounds.'))
-	        } else {
-	          const sel = readlineSync.question(chalk.red('This command will show your password in cleartext. Proceed? (yes): '))
-	          if (sel === 'yes') {
-	            console.log(chalk.cyan.bold(_PASSWORDS[input].password))
-	          } else {
-	            console.log(chalk.green.bold('Command aborted.'))
-	          }
-	        }
-	      } else if (input.startsWith('dpass')) {
-	        input = parseInt(input.slice(5)) - 1
-	        if (input === undefined || isNaN(input) || input < 0 || input >= _PASSWORDS.length) {
-	          console.log(chalk.red.bold('ID out of bounds.'))
-	        } else {
-	          printPass(_PASSWORDS[input], input)
-	          const sel = readlineSync.question(chalk.red.bold('Delete this entry? (yes): '))
-	          if (sel === 'yes') {
-	            _PASSWORDS.splice(input, 1)
-	            console.log(chalk.green.bold('Password deleted Sucessfully.'))
-	          } else {
-	            console.log(chalk.green.bold('Delete aborted.'))
-	          }
-	        }
-	      } else if (input === 'sche') {
-	      		let weak = [], error = false
-			  	for (const i in _PASSWORDS) {
-			  		let strength = false, pwned
-			  		if (passStrength(_PASSWORDS[i].password) !== chalk.green.bold("[STRONG]")) strength = true
-			  		try {
-						pwned = await pwnedPassword(_PASSWORDS[i].password)
-					} catch (err) {
-						console.log(chalk.red.bold("You are not connected to the internet. Krypt needs an internet connection to check if your passwords have been leaked or not."))
-						error = true
-						break
+		loadDatabase()
+		_KEY = crypt.SHA_hash(readlineSync.question('PASSWORD: ', { hideEchoBack: true }))
+		if (_DATABASE.checksum === crypt.SHA_hash(_KEY)) {
+			console.log(chalk.green.bold('Logged in.'))
+			loadPasswords()
+			while (true) {
+				console.log('')
+				let input = readlineSync.prompt()
+				if (input === 'exit') {
+					break
+				} else if (input === 'cmast') {
+					_KEY = crypt.SHA_hash(readlineSync.questionNewPassword())
+					_DATABASE.checksum = crypt.SHA_hash(_KEY)
+					reEncryptData()
+				} else if (input === 'npass') {
+					const name_ = readlineSync.question('Password Name: ')
+					const username_ = readlineSync.question('Username: ')
+					const password_ = readlineSync.question('Password (leave empty to generate): ', { hideEchoBack: true }) || generatePassword()
+					_PASSWORDS.push(createPass(name_, username_, password_))
+					console.log(chalk.green.bold(`Sucessfully added password at ID:${_PASSWORDS.length}.`))
+					reEncryptData()
+					loadDatabase()
+					loadPasswords()
+				} else if (input.startsWith('gent')) {
+					input = parseInt(input.slice(4)) - 1
+					if (input === undefined || isNaN(input) || input < 0 || input >= _PASSWORDS.length) {
+						console.log(chalk.red.bold('ID out of bounds.'))
+					} else {
+						printPass(_PASSWORDS[input], input + 1)
 					}
-			  		if (!error && (strength || pwned)) weak.push({id: i, pwned: pwned})
-			  	}
-			    if (!error) {
-				    for (const i of weak) {
-				    	printPass(_PASSWORDS[i.id], parseInt(i.id) + 1)
-				    	if (i.pwned) console.log(chalk.red.bold(`This password has been seen ${i.pwned} times before!`))
-				    	else
-				    		console.log(chalk.green.bold("This password hasn't been seen before."))
-				    	console.log("")
-				    }
-			    }
-	      } else if (input === 'mpass') {
-	      	const newPass = generatePassword()
-	      	console.log(chalk.cyan.bold(newPass))
-	      	console.log(passStrength(newPass) + chalk.red.bold(`[Occurances:${await pwnedPassword(newPass)}]`))
-	      } else if (input === 'help') {
-	      	console.log(chalk.cyan.bold("Available commands:"))
-	      	for (const comm of _COMMS) console.log(`${chalk.green.bold(comm.name)}: ${chalk.yellow.bold(comm.use)}\nUse: ${chalk.cyan.bold(comm.ex)}\n`)
-	      } else if (input.startsWith('edit')) {
-	        input = parseInt(input.slice(4)) - 1
-	        if (input === undefined || isNaN(input) || input < 0 || input >= _PASSWORDS.length) {
-	          console.log(chalk.red.bold('ID out of bounds.'))
-	        } else {
-	            const name_ = readlineSync.question('Password Name (leave empty to keep same): ')
-		        const username_ = readlineSync.question('Username (leave empty to keep same): ')
-		        const password_ = readlineSync.question('Password (leave empty to generate): ', { hideEchoBack: true }) || generatePassword()
-		        _PASSWORDS[input] = createPass(name_ || _PASSWORDS[input].name, username_ || _PASSWORDS[input].username, password_)
-		        reEncryptData()
-		        loadDatabase()
-		        loadPasswords()
-	        }
-	      } else {
-	        console.log(chalk.red.bold('Invalid command.'))
-	      }
-	    }
-	  } else {
-	    console.log(chalk.red.bold('Wrong Password.'))
-	  }
-	} else {
-	  _DATABASE = { checksum: '', settings: { TwoFA: false }, data: { iv: '', encryptedData: '' } }
-	  _PASSWORDS = []
-	  _KEY = crypt.SHA_hash(readlineSync.questionNewPassword())
-	  _DATABASE.checksum = crypt.SHA_hash(_KEY)
-	  reEncryptData()
+				} else if (input.startsWith('gpass')) {
+					input = parseInt(input.slice(5)) - 1
+					if (input === undefined || isNaN(input) || input < 0 || input >= _PASSWORDS.length) {
+						console.log(chalk.red.bold('ID out of bounds.'))
+					} else {
+						const sel = readlineSync.question(chalk.red('This command will show your password in cleartext. Proceed? (yes): '))
+						if (sel === 'yes') {
+							console.log(chalk.cyan.bold(_PASSWORDS[input].password))
+						} else {
+							console.log(chalk.green.bold('Command aborted.'))
+						}
+					}
+				} else if (input.startsWith('dpass')) {
+					input = parseInt(input.slice(5)) - 1
+					if (input === undefined || isNaN(input) || input < 0 || input >= _PASSWORDS.length) {
+						console.log(chalk.red.bold('ID out of bounds.'))
+					} else {
+						printPass(_PASSWORDS[input], input)
+						const sel = readlineSync.question(chalk.red.bold('Delete this entry? (yes): '))
+						if (sel === 'yes') {
+							_PASSWORDS.splice(input, 1)
+							console.log(chalk.green.bold('Password deleted Sucessfully.'))
+						} else {
+							console.log(chalk.green.bold('Delete aborted.'))
+						}
+					}
+				} else if (input === 'sche') {
+					let weak = [], error = false
+					for (const i in _PASSWORDS) {
+						let strength = false, pwned
+						if (passStrength(_PASSWORDS[i].password) !== chalk.green.bold("[STRONG]")) strength = true
+							try {
+								pwned = await pwnedPassword(_PASSWORDS[i].password)
+							} catch (err) {
+								console.log(chalk.red.bold("You are not connected to the internet. Krypt needs an internet connection to check if your passwords have been leaked or not."))
+								error = true
+								break
+							}
+							if (!error && (strength || pwned)) weak.push({id: i, pwned: pwned})
+						}
+					if (!error) {
+						for (const i of weak) {
+							printPass(_PASSWORDS[i.id], parseInt(i.id) + 1)
+							if (i.pwned) console.log(chalk.red.bold(`This password has been seen ${i.pwned} times before!`))
+								else
+									console.log(chalk.green.bold("This password hasn't been seen before."))
+								console.log("")
+							}
+						}
+					} else if (input === 'mpass') {
+						const newPass = generatePassword()
+						console.log(chalk.cyan.bold(newPass))
+						console.log(passStrength(newPass) + chalk.red.bold(`[Occurances:${await pwnedPassword(newPass)}]`))
+					} else if (input === 'help') {
+						console.log(chalk.cyan.bold("Available commands:"))
+						for (const comm of _COMMS) console.log(`${chalk.green.bold(comm.name)}: ${chalk.yellow.bold(comm.use)}\nUse: ${chalk.cyan.bold(comm.ex)}\n`)
+					} else if (input.startsWith('edit')) {
+						input = parseInt(input.slice(4)) - 1
+						if (input === undefined || isNaN(input) || input < 0 || input >= _PASSWORDS.length) {
+							console.log(chalk.red.bold('ID out of bounds.'))
+						} else {
+							const name_ = readlineSync.question('Password Name (leave empty to keep same): ')
+							const username_ = readlineSync.question('Username (leave empty to keep same): ')
+							const password_ = readlineSync.question('Password (leave empty to generate): ', { hideEchoBack: true }) || generatePassword()
+							_PASSWORDS[input] = createPass(name_ || _PASSWORDS[input].name, username_ || _PASSWORDS[input].username, password_)
+							reEncryptData()
+							loadDatabase()
+							loadPasswords()
+						}
+					} else {
+						console.log(chalk.red.bold('Invalid command.'))
+					}
+				}
+			} else {
+				console.log(chalk.red.bold('Wrong Password.'))
+			}
+		} else {
+			_DATABASE = { checksum: '', settings: { TwoFA: false }, data: { iv: '', encryptedData: '' } }
+			_PASSWORDS = []
+			_KEY = crypt.SHA_hash(readlineSync.questionNewPassword())
+			_DATABASE.checksum = crypt.SHA_hash(_KEY)
+			reEncryptData()
+		}
 	}
-}
 
-main()
+	main()
 
 // important funcs
 
@@ -158,16 +158,16 @@ function generatePassword() {
 		let type = Math.round(Math.random() * 3)
 		switch (type) {
 			case 0:
-				password += _lowerCase[Math.round(Math.random() * 25)]
-				break
+			password += _lowerCase[Math.round(Math.random() * 25)]
+			break
 			case 1:
-				password += _upperCase[Math.round(Math.random() * 25)]
-				break
+			password += _upperCase[Math.round(Math.random() * 25)]
+			break
 			case 2:
-				password += _numbers[Math.round(Math.random() * 9)]
-				break
+			password += _numbers[Math.round(Math.random() * 9)]
+			break
 			case 3:
-				password += _specialChars[Math.round(Math.random() * _specialChars.length)]
+			password += _specialChars[Math.round(Math.random() * _specialChars.length - 1)]
 		}
 	}
 	return password
@@ -180,57 +180,57 @@ function passStrength(pass) {
 
 	if (pass.length < 8) return weak
 
-    var score = 0;
-    if (!pass)
-        return score;
+		var score = 0;
+	if (!pass)
+		return score;
 
-    var letters = new Object();
-    for (var i=0; i<pass.length; i++) {
-        letters[pass[i]] = (letters[pass[i]] || 0) + 1;
-        score += 5.0 / letters[pass[i]];
-    }
+	var letters = new Object();
+	for (var i=0; i<pass.length; i++) {
+		letters[pass[i]] = (letters[pass[i]] || 0) + 1;
+		score += 5.0 / letters[pass[i]];
+	}
 
-    var variations = {
-        digits: /\d/.test(pass),
-        lower: /[a-z]/.test(pass),
-        upper: /[A-Z]/.test(pass),
-        nonWords: /\W/.test(pass),
-    }
+	var variations = {
+		digits: /\d/.test(pass),
+		lower: /[a-z]/.test(pass),
+		upper: /[A-Z]/.test(pass),
+		nonWords: /\W/.test(pass),
+	}
 
-    var variationCount = 0;
-    for (var check in variations) {
-        variationCount += (variations[check] == true) ? 1 : 0;
-    }
-    score += (variationCount - 1) * 10;
+	var variationCount = 0;
+	for (var check in variations) {
+		variationCount += (variations[check] == true) ? 1 : 0;
+	}
+	score += (variationCount - 1) * 10;
 
-    let strength = parseInt(score);
-    if (score > 80) return strong
-    if (score > 60) return med
-    return weak
-}
+	let strength = parseInt(score);
+	if (score > 80) return strong
+		if (score > 60) return med
+			return weak
+	}
 
-function printPass (password, id) {
-  console.log(chalk.blue((`[ID:${id}]${passStrength(password.password)}`)))
-  console.log('Name: ' + chalk.yellow.bold(password.name) + '\n' + 'Username: ' + chalk.yellow.bold(password.username) + '\n' + 'Password: ' + chalk.yellow.bold(new Array(password.password.length + 1).join('*')))
-}
+	function printPass (password, id) {
+		console.log(chalk.blue((`[ID:${id}]${passStrength(password.password)}`)))
+		console.log('Name: ' + chalk.yellow.bold(password.name) + '\n' + 'Username: ' + chalk.yellow.bold(password.username) + '\n' + 'Password: ' + chalk.yellow.bold(new Array(password.password.length + 1).join('*')))
+	}
 
-function createPass (name, username, password) {
-  return { name: name, username: username, password: password }
-}
+	function createPass (name, username, password) {
+		return { name: name, username: username, password: password }
+	}
 
-function loadDatabase () {
-  const data = fs.readFileSync(__dirname + '/../lib/database.json')
-  _DATABASE = JSON.parse(data)
-  return true
-}
+	function loadDatabase () {
+		const data = fs.readFileSync(__dirname + '/../lib/database.json')
+		_DATABASE = JSON.parse(data)
+		return true
+	}
 
-function loadPasswords () {
-  _PASSWORDS = JSON.parse(crypt.AES_decrypt(_DATABASE.data, _KEY))
-  return true
-}
+	function loadPasswords () {
+		_PASSWORDS = JSON.parse(crypt.AES_decrypt(_DATABASE.data, _KEY))
+		return true
+	}
 
-function reEncryptData () {
-  _DATABASE.data = crypt.AES_encrypt(JSON.stringify(_PASSWORDS), _KEY)
-  fs.writeFileSync(__dirname + '/../lib/database.json', JSON.stringify(_DATABASE))
-  return true
-}
+	function reEncryptData () {
+		_DATABASE.data = crypt.AES_encrypt(JSON.stringify(_PASSWORDS), _KEY)
+		fs.writeFileSync(__dirname + '/../lib/database.json', JSON.stringify(_DATABASE))
+		return true
+	}
