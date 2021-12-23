423abb375d234ac223bfef938ae4d189facc085b
Rak Laptudirm <raklaptudirm@gmail.com>
2021-12-23 23:38:36 +0530

    chore: add basic build script

---
+++ script/build.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"time"
+)
+
+func main() {
+	var args []string
+
+	// set env variables from args
+	for _, arg := range os.Args[1:] {
+		idx := strings.IndexRune(arg, '=')
+		if idx >= 0 {
+			os.Setenv(arg[:idx], arg[idx+1:])
+			continue
+		}
+
+		args = append(args, arg)
+	}
+
+	for _, arg := range args {
+		f, ok := tasks[arg]
+		if !ok {
+			fmt.Fprintf(os.Stderr, "Invalid task %v.\n", arg)
+		}
+
+		err := f()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+var tasks = map[string]func() error{
+	"build": build,
+}
+
+func build() error {
+	fmt.Fprintln(os.Stderr, "starting krypt build...")
+	project := "github.com/raklaptudirm/krypt"
+
+	fmt.Fprintln(os.Stderr, "setting ldflags...")
+	ldflags := fmt.Sprintf("-X %s/internal/build.Version=%s", project, version())
+	ldflags = fmt.Sprintf("-X %s/internal/build.Date=%s %s", project, date(), ldflags)
+
+	fmt.Fprintln(os.Stderr, "building executable...")
+	return run("go", "build", "-trimpath", "-ldflags", ldflags, "-o", exe(), "./cmd/krypt")
+}
+
+func exe() string {
+	exe := "bin/krypt"
+	if runtime.GOOS == "windows" {
+		exe += ".exe"
+	}
+
+	return exe
+}
+
+func version() string {
+	desc, err := runOutput("git", "describe", "--tags")
+	if err == nil {
+		return desc
+	}
+
+	rev, _ := runOutput("git", "rev-parse", "--short", "HEAD")
+	return rev
+}
+
+func date() string {
+	t := time.Now()
+	return t.Format("2006-01-02")
+}
+
+func run(args ...string) error {
+	cmd := exec.Command(args[0], args[1:]...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
+func runOutput(args ...string) (string, error) {
+	cmd := exec.Command(args[0], args[1:]...)
+
+	cmd.Stderr = ioutil.Discard
+	out, err := cmd.Output()
+	return strings.TrimSuffix(string(out), "\n"), err
+}
