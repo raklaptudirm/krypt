63be0c105c4e1bcdb74c5d6622bdb2399f4ca1ff
Rak Laptudirm <raklaptudirm@gmail.com>
2022-01-23 16:44:13 +0530

    feat: added completion support for zsh

--- internal/cmd/completion/zsh/zsh.go
+++ internal/cmd/completion/zsh/zsh.go
@@ -11,34 +11,44 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package completion
+package zsh
 
 import (
+	"os"
+
 	"github.com/MakeNowJust/heredoc"
-	"github.com/raklaptudirm/krypt/internal/cmd/completion/bash"
-	"github.com/raklaptudirm/krypt/internal/cmd/completion/fish"
-	"github.com/raklaptudirm/krypt/internal/cmd/completion/pwsh"
-	"github.com/raklaptudirm/krypt/internal/cmd/completion/zsh"
 	"github.com/spf13/cobra"
 )
 
 func NewCmd() *cobra.Command {
 	var cmd = &cobra.Command{
-		Use:   "completion",
-		Short: "completion generates completion scripts for shells",
+		Use:   "zsh",
+		Short: "generate the autocompletion script for zsh",
 		Args:  cobra.NoArgs,
 		Long: heredoc.Doc(`
-			Generate the autocompletion script for krypt for the specified shell.      
-			See each sub-command's help for details on how to use the generated script.
+			Generate the autocompletion script for the zsh shell.
+
+			If shell completion is not already enabled in your environment you will need
+			to enable it.  You can execute the following once:
+			
+			$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+			
+			To load completions for every new session, execute once:
+			# Linux:
+			$ krypt completion zsh > "${fpath[1]}/_krypt"
+			# macOS:
+			$ krypt completion zsh > /usr/local/share/zsh/site-functions/_krypt
+			
+			You will need to start a new shell for this setup to take effect.
 		`),
-		Hidden: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return zsh(cmd.Root())
+		},
 	}
 
-	// supported shells
-	cmd.AddCommand(zsh.NewCmd())
-	cmd.AddCommand(bash.NewCmd())
-	cmd.AddCommand(fish.NewCmd())
-	cmd.AddCommand(pwsh.NewCmd())
-
 	return cmd
+}
+
+func zsh(cmd *cobra.Command) error {
+	return cmd.GenZshCompletion(os.Stdout)
 }
